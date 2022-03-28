c53775d91418571190bd9869bfe05f4c4c42a129
IT21361036 <99160062+IT21361036@users.noreply.github.com>
2022-03-28 04:46:30 +0000

    Tutorial6 completed

--- Box.cpp
+++ Box.cpp
@@ -1,7 +1,26 @@
 #include "Box.h"
 
 // Implement setters and getters
+void Box::setLength(double l){
+  length=l;
+}
+void Box::setWidth(double w){
+  width-w;
+}
+void Box::setHeight(double h){
+  height=h;
+}
+int Box::getLength(){
+  return length;
+}
+int Box::getLength(){
+  return width;
+}
 
+int Box::getLength(){
+  return height;
+}
 // Implemenet the calcVolume() unction
 int Box::calcVolume() {
+  return length*width*height;
 }
--- Student.h
+++ Student.h
@@ -1,9 +1,15 @@
 class Student {
   // private section
+	private:
   //    int studentId
   //    name <- 20 charcters
+    int studentId;
+		char name[20];
   
   // public section
   //    assignDetails() method declaration
   //    display() method declaration
+	public:
+		void assignDetails(int pstudentId, const char pname[]);
+		void Display();
 };
